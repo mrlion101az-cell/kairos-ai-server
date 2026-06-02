86db91470ac77efc1fea2f1bd5492aae9991061a
mrlion101az-cell <mrlion101az@gmail.com>
2026-06-01 17:44:48 -0700

    Update npc_engine.py

--- npc_engine.py
+++ npc_engine.py
@@ -2655,7 +2655,6 @@ NPC_PROFILES: Dict[str, Dict[str, Any]] = {
 "Orion intentionally leaves out critical details.",
 "Some of his advice saves players.",
 "Some of his advice ruins them."
-]
 }
 }
 
